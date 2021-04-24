e8703d7dcdd87ca77d2dab7876605c069c703d54
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-04-24 14:15:47 +0300

    Create a signal for Profile object creation

---
+++ devgram/profiles/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+    print('sender', sender)
+    print('instance', instance)
+
+    if created:
+        Profile.objects.create(user=instance)
