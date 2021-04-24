9bbd056a51f9e88875c5fab06a59a97c49bf98fd
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-04-24 14:52:07 +0300

    Define Relationship model

--- devgram/profiles/models.py
+++ devgram/profiles/models.py
@@ -33,3 +33,20 @@ class Profile(models.Model):
             to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
+
+
+STATUS_CHOICES = [
+    ('send', 'send'),
+    ('accepted', 'accepted')
+]
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender}-{self.receiver}-{self.status}'
