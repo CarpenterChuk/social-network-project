c9aaf320794fb440cde3edfd776dacf348594cde
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-04-26 12:27:32 +0300

    Displaying comments and send post confirmation

--- devgram/posts/views.py
+++ devgram/posts/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render, redirect
 from .models import Post, Like
 from profiles.models import Profile
-
+from .forms import PostModelForm, CommentModelform
 # Create your views here.
 
 
 def posts_comment_create_and_list_view(request):
     qs = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
+
+    # initials
+    p_form = PostModelForm()
+    c_form = CommentModelform()
+    post_added = False
+
+    profile = Profile.objects.get(user=request.user)
+
+    if 'submit_p_form' in request.POST:
+        p_form = PostModelForm(request.POST, request.FILES)
+        if p_form.is_valid():
+            instance = p_form.save(commit=False)
+            instance.author = profile
+            instance.save()
+            p_form = PostModelForm()
+            post_added = True
+
+    if 'submit_c_form' in request.POST:
+        c_form = CommentModelform(request.POST)
+        if c_form.is_valid():
+            instance = c_form.save(commit=False)
+            instance.user = profile
+            instance.post = Post.objects.get(id=request.POST.get('post_id'))
+            instance.save()
+            c_form = CommentModelform()
+
     context = {
         'qs': qs,
         'profile': profile,
+        'p_form': p_form,
+        'c_form': c_form,
+        'post_added': post_added,
     }
 
     return render(request, 'posts/main.html', context)
