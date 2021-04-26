2a1a97e34567ba302df5fb0041895930421a1aef
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-04-26 13:50:37 +0300

    Create views for updating and deleting posts

--- devgram/posts/urls.py
+++ devgram/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import posts_comment_create_and_list_view, like_unlike_post
+from .views import posts_comment_create_and_list_view, like_unlike_post, PostDeleteView, PostUpdateView
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', posts_comment_create_and_list_view, name='main-post-view'),
     path('liked/', like_unlike_post, name='like-post-view'),
+    path('<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('<pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
--- devgram/posts/views.py
+++ devgram/posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .models import Post, Like
 from profiles.models import Profile
 from .forms import PostModelForm, CommentModelform
+from django.views.generic import UpdateView, DeleteView
+from django.contrib import messages
+
+
 # Create your views here.
 
 
@@ -71,3 +76,31 @@ def like_unlike_post(request):
         like.save()
 
     return redirect('posts:main-post-view')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/confirm_del.html'
+    success_url = reverse_lazy('posts:main-post-view')
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if not obj.author.user == self.request.user:
+            messages.warning(self.request, 'You need to be the author of the post in order to delete it')
+        return obj
+
+
+class PostUpdateView(UpdateView):
+    form_class = PostModelForm
+    model = Post
+    template_name = 'posts/update.html'
+    success_url = reverse_lazy('posts:main-post-view')
+
+    def form_valid(self, form):
+        profile = Profile.objects.get(user=self.request.user)
+        if form.instance.author == profile:
+            return super().form_valid(form)
+        else:
+            form.add_error(None, 'You need to be the author of the post in order to update it')
+            return super().form_invalid(form)
