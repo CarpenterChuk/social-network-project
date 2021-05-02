2e85fe8ddb4147010e86e9e46d02d369f589e58e
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-05-02 21:16:59 +0300

    Create a profile detail view

--- devgram/profiles/urls.py
+++ devgram/profiles/urls.py
@@ -4,6 +4,7 @@ from .views import (my_profile_view,
                     profiles_list_view,
                     invite_profiles_list_view,
                     ProfileListView,
+                    ProfileDetailView,
                     send_invitation,
                     remove_from_friends,
                     accept_invitation,
@@ -14,12 +15,13 @@ from .views import (my_profile_view,
 app_name = 'profiles'
 
 urlpatterns = [
+    path('', ProfileListView.as_view(), name='all-profiles-view'),
     path('myprofile/', my_profile_view, name='my-profile-view'),
     path('my-invites/', invites_received_view, name='my-invites-view'),
-    path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'),
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
     path('send-invite/', send_invitation, name='send-invite'),
     path('remove-friend/', remove_from_friends, name='remove-friend'),
+    path('<slug>/', ProfileDetailView.as_view(), name='profile-detail-view'),
     path('my-invites/accept/', accept_invitation, name='accept-invite'),
     path('my-invites/reject/', reject_invitation, name='reject-invite'),
 ]
