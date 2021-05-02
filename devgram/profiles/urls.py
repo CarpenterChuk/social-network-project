7c272cbbcab6168acee1a410b31e60762e9d72c3
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-05-02 19:28:13 +0300

    Create view to accept or reject invites

--- devgram/profiles/urls.py
+++ devgram/profiles/urls.py
@@ -6,6 +6,8 @@ from .views import (my_profile_view,
                     ProfileListView,
                     send_invitation,
                     remove_from_friends,
+                    accept_invitation,
+                    reject_invitation,
                     )
 
 
@@ -18,4 +20,6 @@ urlpatterns = [
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
     path('send-invite/', send_invitation, name='send-invite'),
     path('remove-friend/', remove_from_friends, name='remove-friend'),
+    path('my-invites/accept/', accept_invitation, name='accept-invite'),
+    path('my-invites/reject/', reject_invitation, name='reject-invite'),
 ]
